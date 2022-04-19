04f4b97b23c66203ba76c7818892397dacb189a3
dajiangjunok <779503577@qq.com>
2022-04-19 23:01:33 +0800

    add husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     'eslint:recommended',
     '@vue/typescript/recommended',
     '@vue/prettier',
-    '@vue/prettier/@typescript-eslint',
+    // '@vue/prettier/@typescript-eslint',
     'plugin:prettier/recommended'
   ],
   parserOptions: {
